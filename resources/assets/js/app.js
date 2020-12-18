e314b04d9d9df927dbd7b4d5fe9765df8e71f4a7
Goshetobee <gosho.ginev@gmail.com>
2020-12-18 23:23:14 +0200

    Добавен е bootstrap, jquery и popper.js

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,1 +1,9 @@
 require('./bootstrap');
+import {$,jQuery} from 'jquery';
+
+// export for others scripts to use
+window.$ = $;
+window.jQuery = jQuery;
+
+import * as popper from 'popper.js';
+window.popper = popper;
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,4 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/assets/js')
-    .sass('resources/assets/scss/app.scss', 'public/assets/scss', []);
+    .sass('resources/assets/scss/app.scss', 'public/assets/css', []);
