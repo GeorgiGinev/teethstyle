e8e471380c7171699d2b19130e89656234ed08f6
Goshetobee <gosho.ginev@gmail.com>
2020-12-21 12:54:21 +0200

    Добавен е locale за sitemap view.

--- app/Modules/Pages/Resources/views/sitemap.blade.php
+++ app/Modules/Pages/Resources/views/sitemap.blade.php
@@ -1,5 +1,11 @@
 @extends('welcome')
 
 @section('content')
-
+    <div class="animate__animated animate__fadeIn">
+        <header class="my-4 pb-4">
+            <h1>
+                @lang('pages::sitemap.title')
+            </h1>
+        </header>
+    </div>
 @endsection
