f7c1995f557a6d8555f0b0a7de67dc561e157a9b
Goshetobee <gosho.ginev@gmail.com>
2020-12-23 20:52:55 +0200

    Добавен е gtagmanager.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,15 @@
 
     <link rel="stylesheet" href="{{asset('assets/css/app.css')}}">
     <title>TeethStyle.com</title>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-K3RQQJW7PZ"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-K3RQQJW7PZ');
+    </script>
 </head>
 <body>
     @include('components.header')
